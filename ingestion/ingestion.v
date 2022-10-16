d2253478ec8fafc26b0298a8899dcce756f340f4
Cyklan <dev@cyklan.de>
2022-10-16 12:36:30 +0200

    upload files and get video id

--- ingestion/ingestion.v
+++ ingestion/ingestion.v
@@ -1,5 +1,43 @@
 module main
+import vweb
+import os
+import time
+import invipal.nanoid
+
+const (
+	id_options = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+)
+
+struct App {
+	vweb.Context
+}
 
 fn main() {
-	println('Hello World!')
+	vweb.run(&App{}, 5000)
+}
+
+["/ingest"; post]
+fn (mut app App) ingest_video() vweb.Result {
+	if !("video" in app.files) {
+		app.set_status(400, "")
+		return app.json({
+			"error": "Missing required key: 'video'"
+		})
+	}
+
+	video := app.files["video"][0]
+	filename := video.filename + time.now().unix_time().str()
+
+	id := nanoid.generate(id_options, 12) or { return app.json({ "error": error.str() } )}
+
+	mut file := os.create("./uploads/$filename") or { 
+		return app.json({"error": error.str()}) 
+	}
+	file.write(video.data.bytes()) or {
+		return app.json({"error": error.str()})
+	}
+	file.close()
+	return app.json({
+		"id": id
+	})
 }
